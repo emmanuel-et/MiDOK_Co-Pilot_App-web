27df4132464b489ca8401739be257a93b476b429
emmanuel-et <eietukudoh@gmail.com>
2023-11-19 16:29:57 -0500

    Work on the home card components

--- MiDOK/src/components/HomeCardComponent.tsx
+++ MiDOK/src/components/HomeCardComponent.tsx
@@ -1,8 +1,26 @@
+import '../styles/components/HomeCardComponent.css'
+import student from "../images/student.jpg"
+import HealthProfessional from "../images/doctor.jpg"
+import NonHealthProfessional from "../images/nonpro.png"
 
+const HomeCardComponent = (props : {text : string}) => {
 
-const HomeCardComponent = () => {
-    
-    return <h1>Home Page</h1>
+    const imageMap : Record<string, string> ={
+        "Student": student,
+        "Health Professional": HealthProfessional,
+        "Non-Health Professional": NonHealthProfessional
+    }
+
+    const imgSrc: string = imageMap[props.text];
+
+    return (
+        <>
+            <div className="container">
+                <img className="icon" src={imgSrc} />
+                <p className="text">{props.text}</p>
+            </div>
+        </>
+    );
 };
 
 export default HomeCardComponent;
