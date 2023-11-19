cd0fb502b9bc87ffca8f8d93348c1287b782ebdf
emmanuel-et <eietukudoh@gmail.com>
2023-11-19 17:56:37 -0500

    Add support for routing to user page and touches on home page

--- MiDOK/src/components/HomeCardComponent.tsx
+++ MiDOK/src/components/HomeCardComponent.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import styles from '../styles/components/HomeCardComponent.module.css'
 import student from "../images/student.jpg"
 import HealthProfessional from "../images/doctor.jpg"
 import NonHealthProfessional from "../images/nonpro.png"
 
-const HomeCardComponent = (props : {text : string}) => {
+const HomeCardComponent = (props : {text : string, onClick : React.MouseEventHandler<HTMLDivElement>}) => {
 
     const imageMap : Record<string, string> ={
         "Student": student,
@@ -15,7 +16,7 @@ const HomeCardComponent = (props : {text : string}) => {
 
     return (
         <>
-            <div className={styles.container}>
+            <div className={styles.container} onClick={props.onClick}>
                 <img className={styles.icon} src={imgSrc} />
                 <p className={styles.text}>{props.text}</p>
             </div>
