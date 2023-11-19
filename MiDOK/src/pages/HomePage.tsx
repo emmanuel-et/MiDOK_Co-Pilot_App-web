7018cd24a6685aa54210086d4315b3b9bbba81f3
hacker-elisha <etukudohelisha@gmail.com>
2023-11-19 21:09:46 +0100

    Worked on the disclaimer, MiDOk title, logo, and the question.

--- MiDOK/src/pages/HomePage.tsx
+++ MiDOK/src/pages/HomePage.tsx
@@ -1,7 +1,18 @@
+import Disclaimer from '../components/Disclaimer';
+import '../styles/HomePage.css'
+import logo from "../images/midok.jpg"
 
 const HomePage = () => {
-    
-    return <h1>Home Page</h1>
+    return (
+        <>
+            <div className="container">
+                <p className="midok">MIDOK</p>
+                <img src={logo} />
+                <p className="who">Who are you?</p>
+                <Disclaimer />
+            </div>
+        </>
+    )
 };
 
 export default HomePage;
