77cc63746216b088a0b0d8df145efca22ec92a0e
emmanuel-et <eietukudoh@gmail.com>
2023-11-19 19:27:15 -0500

    Work on user page

--- MiDOK/src/components/HomeCardComponent.tsx
+++ MiDOK/src/components/HomeCardComponent.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from '../styles/components/HomeCardComponent.module.css'
-import student from "../images/student.jpg"
+import Student from "../images/student.jpg"
 import HealthProfessional from "../images/doctor.jpg"
 import NonHealthProfessional from "../images/nonpro.png"
 
 const HomeCardComponent = (props : {text : string, onClick : React.MouseEventHandler<HTMLDivElement>}) => {
 
     const imageMap : Record<string, string> ={
-        "Student": student,
+        "Student": Student,
         "Health Professional": HealthProfessional,
         "Non-Health Professional": NonHealthProfessional
     }
--- MiDOK/src/pages/UserPage.tsx
+++ MiDOK/src/pages/UserPage.tsx
@@ -1,8 +1,39 @@
+import {useParams} from "react-router-dom";
 import CardComponent from "../components/CardComponent";
+import Disclaimer from "../components/Disclaimer";
+import Student from "../images/advising.png"
+import HealthProfessional from "../images/health_professional_image.jpg"
+import NonHealthProfessional from "../images/nonpro.png"
+import styles from '../styles/UserPage.module.css'
+
 
 const UserPage = () => {
+    let userType: string | undefined = useParams().id;
+    let userText: string = "None";
+    let imgSrc: string = "None";
+
+    if (userType === "student") {
+        imgSrc = Student;
+        userText = "Student";
+    } else if (userType === "health-professional") {
+        imgSrc = HealthProfessional;
+        userText = "Health Professional";
+    } else if (userType === "non-health-professional") {
+        imgSrc = NonHealthProfessional;
+        userText = "Non-Health Professional";
+    }
+
     return (
-        <CardComponent text="GENERAL WELL-BEING" />
+        <>  
+            <div className={styles.container}>
+                <img className={styles.image} src={imgSrc} />
+                <p className={styles.text}>{userText}</p>
+                <CardComponent text="GENERAL WELL-BEING" />
+                <CardComponent text="HEALTHCARE INFO" />
+                <CardComponent text="REAL-TIME DATA" />
+                <Disclaimer />
+            </div>
+        </>
     );
 };
 
